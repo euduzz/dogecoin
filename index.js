d694e0236b4797feeb3445f349c3103df9e24102
Eduardo Correia <ecorreia.fit@gmail.com>
2021-03-24 13:38:54 -0300

    remove unused express

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-require('dotenv').config()
-const express = require('express');
-const app = express();
-
 var Bot = require('./tools/bot');
 const schedule = require('node-schedule');
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 schedule.scheduleJob("*/30 7-19 * * *", (async function() {
   Bot.init()
 }))
-
-app.listen(() => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
