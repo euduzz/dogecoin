f766b43ac4db0071d59c97c7265b2f7c2a9e3547
Eduardo Correia <ecorreia.fit@gmail.com>
2021-03-25 09:53:47 -0300

    set new interval

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Bot = require('./tools/bot');
 const schedule = require('node-schedule');
 
-// schedule.scheduleJob("*/30 7-19 * * *", (async function() {
-//   Bot.init()
-// }))
-
-schedule.scheduleJob("* * * * *", (async function() {
+schedule.scheduleJob("*/30 7-19 * * *", (async function() {
   Bot.init()
 }))
